cd3ece49d7e1ed513a4f5e9a73f8790854c72209
Miguel Cazares <miguel.cazares89@gmail.com>
2025-11-05 14:39:19 -0600

    lots of progress

--- KaraokeTime/src/KaraokeTimeMainScreen.tsx
+++ KaraokeTime/src/KaraokeTimeMainScreen.tsx
@@ -1,19 +1,58 @@
-import React from "react";
-import { StatusBar, Text } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { StyleSheet } from "react-native/types_generated/index";
+import React, { useEffect, useState } from "react";
+import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { InfoToast } from 'react-native-toast-message';
 
 function KaraokeTimeMainScreen() {
+    const [ isKaraokeTimeRunning, setIsKaraokeTimeRunning ] = useState(false);
+    const [ songCount, setSongCount ] = useState(0);
+    useEffect(() => {
+        console.log('KaraokeTimeMainScreen mounted');
+
+        return (() => {
+            console.log('KaraokeTimeMainScreen unmounted')
+        });
+    }, []);
+    useEffect(() => {
+        if (isKaraokeTimeRunning) {
+            console.log('Karaoke started');
+        }
+        else if (!isKaraokeTimeRunning) {
+            console.log('Karaoke paused');
+        }
+    }, [isKaraokeTimeRunning]);
     return (
-        <SafeAreaProvider>
+        <SafeAreaView style={styles.container}>
             <StatusBar barStyle={'light-content'} />
-            <Text style={styles.container}>IT'S KARAOKE TIME!!! ✅ 🎤 🎶</Text>
-        </SafeAreaProvider>
+            <View style={styles.spacer} />
+            <TouchableOpacity
+                style={styles.bottomButton}
+                onPress={() => setIsKaraokeTimeRunning(isKaraokeTimeRunning => !isKaraokeTimeRunning)}>
+                    <Text style={styles.bottomButtonText}>{isKaraokeTimeRunning ? 
+                    "IT'S KARAOKE TIME!!! ✅ 🎤 🎶"
+                    : "Karaoke Paused"}</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        justifyContent: 'space-between',
+    },
+    bottomButton: {
+        height: 75,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'gray',
+    },
+    bottomButtonText: {
+        fontSize: 18,
+        color: 'white',
+    },
+    spacer: {
         flex: 1,
     }
 });
